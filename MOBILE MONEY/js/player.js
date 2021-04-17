23c64e41a7322b255ddac27453be4edb64334b52
ARoundCircle <53785163+ARoundCircle@users.noreply.github.com>
2021-04-18 03:54:43 +1000

    Add files via upload

---
+++ MOBILE MONEY/js/player.js
@@ -0,0 +1,18 @@
+
+var player = {
+    money: 10,
+    graphics: 0,
+    marketing: [],
+    artists: [],
+    lootboxTeam: []
+}
+
+var firstMarketing = {
+    cost: 10,
+    mult: 1,
+    amount: 0,
+    bought: 0,
+    name: "Facebook Ad Campaign I"
+}
+
+player.marketing.push(firstMarketing)
