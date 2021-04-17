6f9cc1db6a65317588557f395c3917eb099edbde
ZhiYang <lewzl@student.unimelb.edu.au>
2021-04-18 01:40:33 +0800

    Hello World

---
+++ Game/main.js
@@ -0,0 +1,58 @@
+var cash = 10
+var generators = []
+var lastUpdate = Date.now()
+
+for (let i=0; i<10; i++) {
+    let generator = {
+        cost: Math.pow(Math.pow(10, i), i) * 10,
+        bought: 0,
+        amount: 0,
+        mult: 1
+    }
+    generators.push(generator)
+}
+
+function format(amount) {
+    let power = Math.floor(Math.log10(amount))
+    let mantissa = amount / Math.pow(10, power)
+    if (power < 3) return amount.toFixed(2)
+    return mantissa.toFixed(2) + "e" + power
+}
+
+function buyGenerator(i) {
+    let g = generators[i - 1]
+    if (g.cost > cash) return
+    cash -= g.cost
+    g.amount += 1
+    g.bought += 1
+    g.mult *= 1.1
+    g.cost *= 1.2
+}
+
+function updateGUI() {
+    document.getElementById("cash").textContent = "Cash: " + format(cash)
+    for (let i=0; i<10; i++) {
+        let g = generators[i]
+        document.getElementById("gen" + (i + 1)).innerHTML = "Generator " + (i + 1) + "<br>Amount: " + format(g.amount) + "<br>Bought: " + g.bought + "<br>Multiplier: " + format(g.mult) + "<br>Cost: " + format(g.cost)
+        if (g.cost > cash) document.getElementById("gen" + (i + 1)).classList.add("locked")
+        else document.getElementById("gen" + (i + 1)).classList.remove("locked")
+        if (g.cost > cash && g.bought == 0) document.getElementById("gen" + (i + 1)).classList.add("hidden")
+        else document.getElementById("gen" + (i + 1)).classList.remove("hidden")
+    }
+}
+
+function productionLoop(diff) {
+    cash += generators[0].amount * generators[0].mult * diff
+    for (let i=1; i<10; i++) {
+        generators[i - 1].amount += generators[i].amount * generators[i].mult * diff
+    }
+}
+
+function mainLoop() {
+    var diff = (Date.now() - lastUpdate) / 1000
+    productionLoop(diff)
+    updateGUI()
+    lastUpdate = Date.now()
+}
+
+setInterval(mainLoop, 50)
